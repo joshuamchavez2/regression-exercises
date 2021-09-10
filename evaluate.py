378284a0ec92249defd26283fc791f82fc3c92ed
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-09-09 23:44:24 -0500

    finished up evaluate

---
+++ evaluate.py
@@ -0,0 +1,53 @@
+# Math 
+from math import sqrt
+from scipy import stats
+import statistics
+import os
+
+# General
+import numpy as np
+import pandas as pd
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'sse': sse(actual, predicted),
+        'ess': ess(actual, predicted),
+        'tss': tss(actual),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    })
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'sse': sse(actual, predicted),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    }
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline
+
+def r2_score(actual, predicted):
+    return ess(actual, predicted) / tss(actual)
